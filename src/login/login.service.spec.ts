f28ce8839d07160c0fb0a553e0ea7fc5de2270b9
richiebono <richiebono@gmail.com>
2022-10-26 11:00:08 +0200

    Test to close connections

--- src/login/login.service.spec.ts
+++ src/login/login.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestingModule } from '@nestjs/testing';
 import { LoginService } from './login.service';
 import { loginTestModule } from './login.test.module'
+import { closeInMongodConnection } from '../utils/test/mongo/mongoose.test.module';
 
 describe('LoginService', () => {
   let service: LoginService;
-
+  let module: TestingModule;
   beforeEach(async () => {
-    const module: TestingModule = await loginTestModule.compile();
+    module = await loginTestModule.compile();
     service = module.get<LoginService>(LoginService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  afterAll(async () => {
+    await closeInMongodConnection();    
+    await module.close();
+  });
+  
 });
